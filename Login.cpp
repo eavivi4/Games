ebce1bf848c789dcbb6ad406e8a7650e1ce73751
eavivi4 <avivi.eden@gmail.com>
2022-10-06 18:24:07 -0800

    Fixed scoring system and added hidden password feature

--- Login.cpp
+++ Login.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "RPS.h"
 #include "TTT.h"
 #include "Snake.h"
 using namespace std;
 
 class User {
+
+   // Class User that keeps track of user's information 
    private:
       string username, password;
       int score;
@@ -18,7 +21,6 @@ class User {
       ~User() {
          username = "";
          password = "";
-         //score = 0;
       }
       string GetUsername() {
          return username;
@@ -43,64 +45,38 @@ class User {
       }
 };
 
-bool LoginFunc (string username, string password)
+bool LoginFunc (string username, string password, int &s)
 {
-   string e_username, e_password;
-   // If username exists
+   string f_username, f_password, f_score;
+
+   // If username exists, check username and password and store score in variable
    ifstream file(username + ".txt");
-   getline(file, e_username);
-   getline(file, e_password);
-   if (username == e_username && password == e_password)
+   getline(file, f_username);
+   getline(file, f_password);
+   getline(file, f_score);
+   if (username == f_username && password == f_password)
    {
+      s = stoi(f_score);
       return true;
    }
    return false;
 }
 
-void RegisterFunc(string username, string password)
+void RegisterFunc(string username, string password, int s)
 {
-   // If username does not exist
+   // Register the user or just insert the score
    ofstream file;
    file.open(username + ".txt");
-   file << username << endl << password;
+   file << username << endl << password << endl << s;
    file.close();
+   return;
 }
 
-int main() {
-   string option, username, password;
+void Hub(User mainUser) {
+   string answer;
    int pick;
-   User mainUser = User();
-   cout << "Welcome to the GameHub" << endl;
-   cout << "Would you like to login or register? ";
-   // Input validation missing?
-   cin >> option;
 
-   cout << "Please Enter a username: ";
-   cin >> username;
-   // Have password hidden?
-   cout << "Please Enter a password: ";
-   cin >> password;
-
-   // Register or login
-   if (option == "register")
-   {
-      RegisterFunc(username, password);
-   }
-   else if (option == "login" && !LoginFunc(username, password))
-   {
-      cout << "Wrong username or password..." << endl;
-      main();
-   }
-   else if (option != "login")
-   {
-      cout << "No such option..." << endl;
-      main();
-   }
-
-   mainUser.SetUsername(username);
-   mainUser.SetPassword(password);
-
-   // Only arrive here if registered or correct login info
+   // Either user is logged in or needs to register later to arrive in the hub
    cout << "-----------------------------------------" << endl;
    cout << "Welcome " << mainUser.GetUsername() << "!" << endl;
    cout << "Your current score is: " << mainUser.GetScore() << endl;
@@ -111,6 +87,8 @@ int main() {
    cout << "3. Snake" << endl;
    cout << "4. Exit" << endl;
    cin >> pick;
+
+   // Choose the game based on user input
    switch (pick)
    {
       case 1:
@@ -128,9 +106,78 @@ int main() {
    }
 
    // Have score inserted into file for user so it is remembered for next time
+   RegisterFunc(mainUser.GetUsername(), mainUser.GetPassword(), mainUser.GetScore());
 
-   // Clear user info?
-   username = "";
-   password = "";
+   // Ask user if to return to hub for another game if they didn't exit
+   if (pick != 4)
+   {
+      cout << "Would you like to play another game from the hub? yes/no" << endl;
+      cin >> answer;  
+      if (answer == "yes")
+      {
+         // If the user did not choose to exit, continue with prompt
+         Hub(mainUser);
+      }
+      
+      // Any other answer, except for y, would result in exiting the hub
+      else if (answer == "no")
+      {
+         return;
+      }
+      else
+      {
+         cout << "Invalid answer, exiting the hub" << endl;
+         return;
+      }
+   }
+   return;
+   
+}
+
+int main() {
+   string option, username, password;
+   int score = 0;
+   User mainUser = User();
+
+   cout << "Welcome to the GameHub" << endl;
+   cout << "Would you like to login or register? ";
+   cin >> option;
+   if (option != "login" && option != "register")
+   {
+      // Input validation
+      cout << "No such option..." << endl;
+      main();
+   }
+
+   cout << "Please Enter a username: ";
+   cin >> username;
+   
+   char ch;
+   cout << "Please Enter a password: ";
+
+   // Have password hidden, while it is not enter
+   ch = getch();
+   while (ch != 13)
+   {
+      password.push_back(ch);
+      cout << "*";
+      ch = getch();
+   }
+   cout << endl;
+
+   // Check if login is correct, if selected login
+   if (option == "login" && !LoginFunc(username, password, score))
+   {
+      cout << "Wrong username or password..." << endl;
+      main();
+   }
+
+   // Setup the object
+   mainUser.SetUsername(username);
+   mainUser.SetPassword(password);
+   mainUser.AddScore(score);
+
+   Hub(mainUser);
+
    return 0;
 }
